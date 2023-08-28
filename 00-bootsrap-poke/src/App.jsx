7d737bb0d44df7786e909d137b35fe5dfc85fdee
JuanSRovira <narukankid@gmail.com>
2023-08-28 12:40:54 -0600

    boostrap NAV

---
+++ 00-bootsrap-poke/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Home from './Pages/Home'
+import NavBar from './components/NavBar'
+
+
+function App() {
+  return (
+    <>
+    <NavBar />
+    <Home />
+    </>
+  )
+}
+
+export default App
